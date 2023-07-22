28d699acd4122ba510c192bf0c752690c1e1e94b
imampamuji <imampamuji.2021@student.uny.ac.id>
2023-07-22 19:42:44 +0700

    3. Struktur Folder, Routes & View

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman Post</title>
+</head>
+<body>
+    <h1>HALAMAN POST</h1>
+    <p><?= $name;?></p>
+    <p><?= $email;?></p>
+    <img src="img/<?= $image;?>" alt="<?= $name;?>" width="200px">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/posts', function () {
+    return view('posts', [
+        "name" => "Imam Pamuji",
+        "email" => "imam275pamuji@gmail.com",
+        "image" => "imampamuji.png"
+    ]);
 });
